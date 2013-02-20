12cdc02f15f41ea66235158c465c77f57abe7034
mzhaoTWer <zm_uestc@hotmail.com>
2013-02-20 20:39:31 +0800

    finished Story 3, but some much duplication

--- src/java/com/kata/AccountResult.java
+++ src/java/com/kata/AccountResult.java
@@ -1,29 +1,51 @@
 package com.kata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AccountResult {
-    public static final String ILLEGIBLE_FLAG = "ILL";
-    public static final String ERROR_FLAG = "ERR";
-    private final Output output;
-    private final Checksum checksum;
-    private String account;
+
+    public static final String ILLEGIBLE_FLAG = " ILL";
+    public static final String ERROR_FLAG = " ERR";
+
+    private Output output;
+    private Checksum checksum;
     private EntryParser entryParser;
+
+    private final List<String> accounts = new ArrayList<String>();
+    private String account;
 
     public AccountResult(String originalAccount, Output output) {
         this.entryParser = new EntryParser();
-        this.account = entryParser.parse(originalAccount);
-        this.output = output;
         this.checksum = new Checksum();
+        this.output = output;
+        this.account = entryParser.parse(originalAccount);
+    }
+
+    public AccountResult(List<String> accounts, Output output) {
+        this.entryParser = new EntryParser();
+        this.checksum = new Checksum();
+        this.output = output;
+        for (String account : accounts) {
+            this.accounts.add(entryParser.parse(account));
+        }
     }
 
     public void printAccountResult() {
-        if(account.contains(entryParser.SUBSTITUTE_WHEN_ILLEGIBLE))  {
-            output.print(account + " " + ILLEGIBLE_FLAG);
+        if (account.contains(entryParser.SUBSTITUTE_WHEN_ILLEGIBLE)) {
+            output.print(account + ILLEGIBLE_FLAG);
             return;
         }
-        if (checksum.validate(account)) {
-            output.print(account);
-        } else {
-            output.print(account + " " + ERROR_FLAG);
+        output.print((checksum.validate(account)) ? account : (account + ERROR_FLAG));
+    }
+
+    public void printAccountsResult() {
+        for (String account : this.accounts) {
+            if (account.contains(entryParser.SUBSTITUTE_WHEN_ILLEGIBLE)) {
+                output.print(account + ILLEGIBLE_FLAG);
+                return;
+            }
+            output.print((checksum.validate(account)) ? account : (account + ERROR_FLAG));
         }
     }
 }
--- src/test/com/kata/AccountResultTest.java
+++ src/test/com/kata/AccountResultTest.java
@@ -3,15 +3,18 @@ package com.kata;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 /*
  * Tasking:
- * 1) Print a account
- * 2) Print a correct account with checksum
- * 3) Print a ERR account
- * 4) Print a ILL account
+ * 1) Print a correct account with checksum
+ * 2) Print a ERR account
+ * 3) Print a ILL account
+ * 4) Print more than one account(In progress)
  */
 
 public class AccountResultTest {
@@ -21,22 +24,6 @@ public class AccountResultTest {
     @Before
     public void setUp() throws Exception {
         mockOutput = mock(Output.class);
-    }
-
-    @Test
-    public void print_whatever_account() throws Exception {
-        //given
-        String accountSource = "" +
-                "    _  _     _  _  _  _  _ " +
-                "  | _| _||_||_ |_   ||_||_|" +
-                "  ||_  _|  | _||_|  ||_| _|";
-
-
-        //when
-        new AccountResult(accountSource, mockOutput).printAccountResult();
-
-        //then
-        verify(mockOutput).print("123456789");
     }
 
     @Test
@@ -84,6 +71,36 @@ public class AccountResultTest {
         new AccountResult(accountSource, mockOutput).printAccountResult();
 
         //then
+        verify(mockOutput).print("86110??36 ILL");
+    }
+
+    @Test
+    public void print_account_array_with_checksum() throws Exception {
+        //given
+        String accountSource1 =
+                "" +
+                        "    _  _  _  _  _  _  _  _ " +
+                        "|_||_   ||_ | ||_|| || || |" +
+                        "  | _|  | _||_||_||_||_||_|";
+        String accountSource2 =
+                "" +
+                        " _  _     _  _        _  _ " +
+                        "|_ |_ |_| _|  |  ||_||_||_ " +
+                        "|_||_|  | _|  |  |  | _| _|";
+        String accountSource3 =
+                "" +
+                        " _  _        _        _  _ " +
+                        "|_||_   |  || || ||_  _||_ " +
+                        "|_||_|  |  ||_| _|| | _||_|";
+
+        List<String> accounts = Arrays.asList(accountSource1, accountSource2, accountSource3);
+
+        //when
+        new AccountResult(accounts, mockOutput).printAccountsResult();
+
+        //then
+        verify(mockOutput).print("457508000");
+        verify(mockOutput).print("664371495 ERR");
         verify(mockOutput).print("86110??36 ILL");
     }
 }
